2c46054556f1bc3c100145222872e8c37047ca02
celliottcmtn <elliott.colin@gmail.com>
2025-03-06 14:18:09 -0800

    Update car_market_game.py

--- car_market_game.py
+++ car_market_game.py
@@ -187,7 +187,7 @@ if st.sidebar.button("Simulate Market"):
             <p><strong>New Estimated Profit:</strong> ${tariffed_profit:,.2f}</p>
         </div>
         """, unsafe_allow_html=True)
-                        tariffed_cost = (speed * 2000) + (aesthetics * 1500) + (reliability * 1800) + (efficiency * 1700) + (tech * 2500)
+                                tariffed_cost = (speed * 2000) + (aesthetics * 1500) + (reliability * 1800) + (efficiency * 1700) + (tech * 2500)
                                 tariffed_cost *= 1.25  # Adding 25% tariff
                                 tariffed_profit = st.session_state['result']['Estimated Sales'] * (price - tariffed_cost)
         
